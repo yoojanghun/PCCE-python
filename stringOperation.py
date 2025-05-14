4c00697e2155285c458278fc60913e5ae48b9375
banana-coder631 <157185025+yoojanghun@users.noreply.github.com>
2025-05-14 17:40:19 +0900

    [3-2.문자열과 데이터] - 여러 항목 출력, 문자열 연산

---
+++ stringOperation.py
@@ -0,0 +1,20 @@
+# 여러 항목 출력: print() 안에서 출력 내용을 ,로 구분
+# 한 칸 띄어쓰기 적용됨
+print("Chocolate", "Candy", "Ice cream")
+
+food1 = "Chocolate"
+food2 = "Candy"
+food3 = "Ice creame"
+
+print("I want to eat", food1, food2, food3)
+
+# 변수 이름은 숫자로 시작 X, 변수명은 소문자
+
+# 문자열 연산: 문자열끼리 덧셈 연산 가능
+# 한 칸 띄어쓰기 적용 X
+string1 = "Samsung" + "phone"
+string2 = "My"
+string3 = string2 + string1
+
+print(string1)
+print(string3)
