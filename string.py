ee44babefeec0a7f4db2c575c86ea9d360a859cd
banana-coder631 <157185025+yoojanghun@users.noreply.github.com>
2025-05-14 17:26:42 +0900

    [3-2.문자열과 데이터] - 문자열

---
+++ string.py
@@ -0,0 +1,13 @@
+# 문자열 (string): 문자 형태의 데이터
+# " 또는 ' 로 문자열의 시작과 끝을 나타냄
+
+# 문자열 내에 큰 따옴표나 작은 따옴표를 사용하기 위해 "와 '를 달리 씀
+print("He thinks, 'I am hungry...'")
+print('She says, "You should go home"')
+
+# 또는 이스케이프 문자를 사용함
+print('He thinks, \'I am hungry...\'')
+print("She says, \"You should go home\"")
+
+# 줄바꿈 이스케이프 문자
+print("I want to push \nEnter")
