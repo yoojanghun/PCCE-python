4399dc00a200c7ce9677d0105a25652a415311e3
banana-coder631 <157185025+yoojanghun@users.noreply.github.com>
2025-05-14 17:32:38 +0900

    [3-2.문자열과 데이터] - 변수

---
+++ variables.py
@@ -0,0 +1,12 @@
+# 변수(Variable): 데이터를 저장할 수 있는 공간
+# 이름과 데이터를 가지고 있다
+# 대입 연산자: 변수에 데이터를 저장할 때 사용하는 기호 "="
+# 변수이름 = 데이터
+
+name = "janghun"
+speak1 = "I like coding"
+speak2 = "I am genius"
+
+print(name)
+print(speak1)
+print(speak2)
