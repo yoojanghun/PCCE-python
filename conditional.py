b74cc2c7d00f5a10cc65fd23032060a1728bc986
yoojanghun <157185025+yoojanghun@users.noreply.github.com>
2025-05-15 09:45:53 +0900

    [3-5.조건문] - 논리 데이터와 비교연산

---
+++ conditional.py
@@ -0,0 +1,15 @@
+# 논리 데이터(Boolean) 참(True) 또는 거짓(False)의 값을 가지는 데이터
+# 비교 연산: 두 데이터의 크기를 비교해서 논리데이터로 반환
+
+x = 8
+y = 5
+print(x < y)
+print(x <= y)
+print(x < 10)
+print(x > y)
+
+str1 = "hello"
+str2 = "hell"
+print(str1 == str2)
+print(str1 != str2)
+print(str1 == "hello")
