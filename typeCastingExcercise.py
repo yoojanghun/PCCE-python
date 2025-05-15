733fc3dd3111d2465ba488f68d5fa13d4e05b6b0
yoojanghun <157185025+yoojanghun@users.noreply.github.com>
2025-05-15 09:40:57 +0900

    [3-4.자료형 변환] - 자료형 변환 실습문제

---
+++ typeCastingExcercise.py
@@ -0,0 +1,19 @@
+# 두 개의 정수를 입력 받고, 더한 결과를 출력하는 프로그램 작성
+a = int(input("첫 번째 숫자를 입력하세요: "))
+b = int(input("두 번째 숫자를 입력하세요: "))
+
+print(a + b)
+
+# 출력 결과를 100200300으로 변경하기
+number1 = str(100)
+number2 = str(200)
+number3 = str(300)
+result = number1 + number2 + number3
+
+print(result)
+
+# 과일 이름과 출력 횟수를 정수로 입력 받고, 출력 횟수만큼 과일 이름 출력
+fruit = input("과일 이름을 입력하세요: ")
+number = int(input("출력할 횟수를 입력하세요: "))
+
+print(fruit * number)
