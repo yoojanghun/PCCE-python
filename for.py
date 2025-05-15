38f6a2fd38216d29aacf5ca3c520991868990627
yoojanghun <157185025+yoojanghun@users.noreply.github.com>
2025-05-15 14:07:02 +0900

    [3-6.반복문] - for

---
+++ for.py
@@ -0,0 +1,11 @@
+# 반복문: 명령을 반복해서 수행하게 만들 때 사용
+# for: 반복 횟수가 정해져 있을 때
+# while: 반복 횟수가 정해져 있지 않을 때
+for i in range(5):
+    print("Learn python")
+
+for i in range(5):
+    print(i)
+
+for i in range(5):
+    print(i+1, "번째 작업 중")
