91e8000c4728751b8d866e6b45e788243cbe9066
yoojanghun <157185025+yoojanghun@users.noreply.github.com>
2025-05-15 09:32:30 +0900

    [3-4.자료형 변환] - 문자열 연산

---
+++ stringOperation2.py
@@ -0,0 +1,12 @@
+# 문자열 * 숫자: 문자열을 숫자만큼 이어준다.
+
+print("janghun" * 2)
+
+# 아무것도 없는 문자열이 출력 된다.
+print("janghun" * 0)
+
+# 아래는 오류. int만 곱할 수 있다.
+# print("janghun" * 2.0)
+
+# 아래는 오류. 나눌 수 없다.
+# print("janghun" / 2)
