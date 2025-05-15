08a7bcd94aca6ce3d53aaec409b7613dc2b78d9e
yoojanghun <157185025+yoojanghun@users.noreply.github.com>
2025-05-15 14:15:18 +0900

    [3-6.반복문] - range

---
+++ range.py
@@ -0,0 +1,13 @@
+# range(start, end)
+# range(start, end, step)
+
+for i in range(1, 6):
+    print(i, "페이지 작업 중")
+print()
+
+for i in range(1, 10, 2):
+    print(i)
+print()
+
+for i in range(10, 0, -1):
+    print(i)
